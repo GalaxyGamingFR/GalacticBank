53f776e839bb3e7da64932c223417fafc745acea
GalaxyGamingFR <therealtariqkhalif@gmail.com>
2026-03-07 00:21:35 -0800

    started

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Main { // class name should always start with an uppercase first letter
+    // class names are case-sensitive
+    // name of class MUST be the same as the class name
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello " + name);
+    }
+
+    static void userBank() {
+        final int userID;
+        int userBalance;
+    }
+
+    int userID;
+    int userBalance;
+
+    static void withdraw(int userID, int amount) {
+
+    }
+
+    static void deposit(int userID, int amount) {}
+
+    static float checkBalance(int userID) {
+        return 0f;
+    }
+}
